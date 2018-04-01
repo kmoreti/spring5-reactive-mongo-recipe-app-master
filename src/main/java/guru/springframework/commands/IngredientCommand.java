d65380444c3f43941c5b5b5df247d6dc38d204d4
kmoreti <kleber.camargo@fatec.sp.gov.br>
2018-04-01 17:57:49 -0300

    Converting application to use reactive technology.

--- src/main/java/guru/springframework/commands/IngredientCommand.java
+++ src/main/java/guru/springframework/commands/IngredientCommand.java
@@ -4,6 +4,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 
 /**
@@ -13,9 +16,17 @@ import java.math.BigDecimal;
 @Setter
 @NoArgsConstructor
 public class IngredientCommand {
+
     private String id;
     private String recipeId;
+
+    @NotBlank
     private String description;
+
+    @NotNull
+    @Min(1)
     private BigDecimal amount;
+
+    @NotNull
     private UnitOfMeasureCommand uom;
 }
--- src/main/java/guru/springframework/controllers/RecipeController.java
+++ src/main/java/guru/springframework/controllers/RecipeController.java
@@ -1,13 +1,16 @@
 package guru.springframework.controllers;
 
 import guru.springframework.commands.RecipeCommand;
+import guru.springframework.exceptions.NotFoundException;
 import guru.springframework.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
+import org.thymeleaf.exceptions.TemplateInputException;
 
 /**
  * Created by jt on 6/19/17.
@@ -79,19 +82,16 @@ public class RecipeController {
         return "redirect:/";
     }
 
-//    @ResponseStatus(HttpStatus.NOT_FOUND)
-//    @ExceptionHandler(NotFoundException.class)
-//    public ModelAndView handleNotFound(final Exception exception) {
-//
-//        log.error("Handling not found exception");
-//        log.error(exception.getMessage());
-//
-//        final ModelAndView modelAndView = new ModelAndView();
-//
-//        modelAndView.setViewName("404error");
-//        modelAndView.addObject("exception", exception);
-//
-//        return modelAndView;
-//    }
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler({NotFoundException.class, TemplateInputException.class})
+    public String handleNotFound(final Exception exception, final Model model) {
+
+        log.error("Handling not found exception");
+        log.error(exception.getMessage());
+
+        model.addAttribute("exception", exception);
+
+        return "404error";
+    }
 
 }
